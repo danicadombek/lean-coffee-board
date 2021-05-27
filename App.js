85550f28da5d67d269158e3e255ca50b6a0f8923
Danica Dombek <danica.dombek@gmx.de>
2021-05-27 14:55:03 +0200

    Modify app.js

--- App.js
+++ App.js
@@ -5,14 +5,17 @@ const assert = require('assert')
 const url = 'mongodb://localhost:27017'
 
 // Database Name
-const dbName = 'lean-coffee-boardf'
-const client = new MongoClient(url)
+const dbName = 'lean-coffee-board'
+
 // Use connect method to connect to the server
-client.connect(function (err) {
-  assert.equal(null, err)
-  console.log('Connected successfully to server')
+MongoClient.connect(url, function (err, client) {
+  assert.strictEqual(null, err)
+  console.log('Connected correctly to server')
 
   const db = client.db(dbName)
+  const collection = db.collection('users')
+
+  collection.insertOne({ name: 'Danica', age: '39', email: 'dd@gmx.de' })
 
   client.close()
 })
