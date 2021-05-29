4f8834490e7215e9820611c46e5fe852aa067350
Danica Dombek <danica.dombek@gmx.de>
2021-05-29 18:07:17 +0200

    Create default 0 for votes in card.js and update error.js

--- models/Card.js
+++ models/Card.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const cardSchema = {
-  title: { type: String, required: true },
+  text: { type: String, required: true },
   author: { type: String, required: true },
-  votes: { type: Number },
+  votes: { type: Number, default: 0 },
 }
 
 module.exports = mongoose.model('Card', cardSchema)
