55db24deb32a5a881acd1b4182fae4df6cc44474
Danica Dombek <danica.dombek@gmx.de>
2021-05-30 22:20:57 +0200

    Give author in card-model objectId and ref to user-model

--- models/Card.js
+++ models/Card.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const cardSchema = {
   text: { type: String, required: true },
-  author: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   votes: { type: Number, default: 0 },
 }
 
